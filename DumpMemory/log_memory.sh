02c02e20a86cb2267f996ea85457febeb7c0990f
hydrated <hydrawork@gmail.com>
2016-08-01 19:07:27 +0800

    Also logcat critical events to file

--- DumpMemory/log_memory.sh
+++ DumpMemory/log_memory.sh
@@ -2,6 +2,8 @@
 # Hydra wrote this software. As long as you retain this notice you can do whatever you want with this stuff. If we meet some day, and you think this stuff is worth it, you can buy me a beer in return.
 
 #!/system/bin/sh
+
+#check input variables
 if [ -n "$1" ]
 then
 echo "Process $1"
@@ -9,16 +11,23 @@ else
 echo "Usage : sh log_memory.sh (process name)"
 exit
 fi
+
+#initialize
 mkdir -p hprof
 MYPID=`pgrep $1`
 echo ${MYPID}
 FILENAME="memorylog"`date +%s`.log
 echo "FILENAME is "${FILENAME}
+
+#logcat
+logcat Androidplugin ActivityManager:I *:S >> ${FILENAME} &
+
+#loop log
 while true; do
 HEAPDIC="hprof/"`date +%s`.hprof
 date >> ${FILENAME}
-dumpsys meminfo ${MYPID} >> ${FILENAME} 2>&1
 procrank >> ${FILENAME}
+dumpsys meminfo ${MYPID} >> ${FILENAME} 2>&1
 am dumpheap ${MYPID} ${HEAPDIC}
 echo >> ${FILENAME}
 echo '\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>' >> ${FILENAME}
