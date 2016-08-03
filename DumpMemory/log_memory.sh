8ece9b03235785a3effacd66be136ffa3e8133c8
hydrated <hydrated@users.noreply.github.com>
2016-08-03 11:52:09 +0800

    Update log_memory.sh

--- DumpMemory/log_memory.sh
+++ DumpMemory/log_memory.sh
@@ -1,5 +1,6 @@
 # log_memory Copyright (c) 2016 Hydra Chang [hydrated@msn.com] 'THE BEER-WARE LICENSE'(Revision 42):
-# Hydra wrote this software. As long as you retain this notice you can do whatever you want with this stuff. If we meet some day, and you think this stuff is worth it, you can buy me a beer in return.
+# Hydra wrote this software. As long as you retain this notice you can do whatever you want with this stuff. 
+# If we meet some day, and you think this stuff is worth it, you can buy me a beer in return.
 
 #!/system/bin/sh
 
